b39f139da1b4afd95f2366420917d38d50ab6381
gokrusk <nigelljama@gmail.com>
2023-06-04 15:41:57 -0500

    home report

--- ReportesForm.Designer.cs
+++ ReportesForm.Designer.cs
@@ -25,13 +25,17 @@
         private void InitializeComponent() {
             this.components = new System.ComponentModel.Container();
             Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource2 = new Microsoft.Reporting.WinForms.ReportDataSource();
             this.tblClienteBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.dataSetVenta = new WinApp_Homes.DataSetVenta();
+            this.TblInmuebleBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.LblIntruccion = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.reportViewer2 = new Microsoft.Reporting.WinForms.ReportViewer();
             ((System.ComponentModel.ISupportInitialize)(this.tblClienteBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataSetVenta)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TblInmuebleBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // tblClienteBindingSource
@@ -44,6 +48,11 @@
             this.dataSetVenta.DataSetName = "DataSetVenta";
             this.dataSetVenta.EnforceConstraints = false;
             this.dataSetVenta.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // TblInmuebleBindingSource
+            // 
+            this.TblInmuebleBindingSource.DataMember = "TblInmueble";
+            this.TblInmuebleBindingSource.DataSource = this.dataSetVenta;
             // 
             // LblIntruccion
             // 
@@ -60,11 +69,13 @@
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
             "Listado de clientes",
+            "Listado de inmuebles",
             "Promedio de precio"});
             this.comboBox1.Location = new System.Drawing.Point(211, 24);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(183, 26);
             this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // reportViewer1
             // 
@@ -77,6 +88,20 @@
             this.reportViewer1.ServerReport.BearerToken = null;
             this.reportViewer1.Size = new System.Drawing.Size(887, 443);
             this.reportViewer1.TabIndex = 2;
+            this.reportViewer1.Visible = false;
+            // 
+            // reportViewer2
+            // 
+            reportDataSource2.Name = "DataSet1";
+            reportDataSource2.Value = this.TblInmuebleBindingSource;
+            this.reportViewer2.LocalReport.DataSources.Add(reportDataSource2);
+            this.reportViewer2.LocalReport.ReportEmbeddedResource = "WinApp_Homes.ReportHomes.rdlc";
+            this.reportViewer2.Location = new System.Drawing.Point(-1, 69);
+            this.reportViewer2.Name = "reportViewer2";
+            this.reportViewer2.ServerReport.BearerToken = null;
+            this.reportViewer2.Size = new System.Drawing.Size(887, 443);
+            this.reportViewer2.TabIndex = 3;
+            this.reportViewer2.Visible = false;
             // 
             // ReportesForm
             // 
@@ -84,6 +109,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(58)))), ((int)(((byte)(63)))));
             this.ClientSize = new System.Drawing.Size(884, 511);
+            this.Controls.Add(this.reportViewer2);
             this.Controls.Add(this.reportViewer1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.LblIntruccion);
@@ -97,6 +123,7 @@
             this.Load += new System.EventHandler(this.ReportesForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.tblClienteBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataSetVenta)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TblInmuebleBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -109,5 +136,7 @@
         private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
         private System.Windows.Forms.BindingSource tblClienteBindingSource;
         private DataSetVenta dataSetVenta;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer2;
+        private System.Windows.Forms.BindingSource TblInmuebleBindingSource;
     }
 }
--- ReportesForm.cs
+++ ReportesForm.cs
@@ -8,17 +8,35 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WinApp_Homes {
-    public partial class ReportesForm : Form {
+namespace WinApp_Homes
+{
+    public partial class ReportesForm : Form
+    {
         readonly string PathFile = Application.StartupPath + "\\assets\\files\\";
 
-        public ReportesForm() {
+        public ReportesForm()
+        {
             InitializeComponent();
             dataSetVenta.ReadXml(PathFile + "clientes.xml");
         }
 
-        private void ReportesForm_Load(object sender, EventArgs e) {
+        private void ReportesForm_Load(object sender, EventArgs e)
+        {
             this.reportViewer1.RefreshReport();
+            this.reportViewer2.RefreshReport();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem.ToString() == "Listado de clientes")
+            {
+                reportViewer1.Visible = true; 
+
+            } else if(comboBox1.SelectedItem.ToString() == "Listado de inmuebles")
+            {
+                reportViewer1.Visible = false;
+                reportViewer2.Visible = true;
+            }
         }
     }
 }
